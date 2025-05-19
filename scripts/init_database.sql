bbeac80d77023b412efd83115c351850c5fe6959
andresvilla06 <150623726+andresvilla06@users.noreply.github.com>
2025-05-19 15:38:47 -0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,28 @@
+/*	DATAWAREHOUSE Proyect 1
+  script purpose: This script is for create a new database but before checking it firts if exist. Then create the
+  schemas for the database.
+
+	--Bronze Layer--
+
+*/
+-------------------------------------------------------------------------------------
+--Checking first if the database exists
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWareHouse')
+  BEGIN
+    ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWareHouse;
+END;
+GO
+  
+--1. (Creating Database)
+CREATE DATABASE DataWareHouse;
+
+--2. (Creating Schemas for the new database)
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
+--3. 
